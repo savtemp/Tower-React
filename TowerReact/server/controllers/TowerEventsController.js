fa977bdc8ab350641d4601ffa743379fafe5d3a3
savtemp <savtemple@gmail.com>
2023-03-24 12:47:21 -0600

    started createTowerEvent in controller - unfinished

--- TowerReact/server/controllers/TowerEventsController.js
+++ TowerReact/server/controllers/TowerEventsController.js
@@ -8,12 +8,22 @@ export class TowerEventsController extends BaseController{
     this.router
       // routes
       .get('', this.getTowerEvents)
+      .post('', this.createTowerEvent)
   }
 
   async getTowerEvents (req, res, next) {
   try {
     const events = await towerEventsService.getTowerEvents()
     return res.send(events)
+  } catch (error) {
+    next(error)
+    }
+  }
+
+  async createTowerEvent (req, res, next) {
+  try {
+    
+    return res.send()
   } catch (error) {
     next(error)
     }
