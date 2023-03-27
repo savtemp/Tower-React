fcc4c463fea67b43804c7130b49391a8c23e1eae
savtemp <savtemple@gmail.com>
2023-03-27 11:44:10 -0600

    getById and create methods added to controller

--- TowerReact/server/controllers/TowerEventsController.js
+++ TowerReact/server/controllers/TowerEventsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { towerEventsService } from "../services/TowerEventsService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,9 +7,25 @@ export class TowerEventsController extends BaseController{
   constructor(){
     super(`api/events`)
     this.router
-      // routes
+      // Routes
+      // get all the events
       .get('', this.getTowerEvents)
+      // get an event by its Id
+      .get('/:id', this.getTowerEventById)
+      // get the event comments
+      // .get('/:id/comments', this.getTowerEventComments)
+      // get the event tickets
+      // .get('/:id/tickets', this.getTowerEventTickets)
+
+      // NOTE Authorized Routes
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      // create an event
       .post('', this.createTowerEvent)
+      // edit an event
+      // .put('/:id', this.editEvent)
+      // delete an event 
+      // .delete('/:id', this.cancelEvent)
+
   }
 
   async getTowerEvents (req, res, next) {
@@ -22,10 +39,23 @@ export class TowerEventsController extends BaseController{
 
   async createTowerEvent (req, res, next) {
   try {
-    
-    return res.send()
+    req.body.creatorId = req.userInfo.id
+    let eventData = req.body
+    const newEvent = await towerEventsService.createTowerEvent(eventData)
+    return res.send(newEvent)
   } catch (error) {
     next(error)
     }
   }
+
+  async getTowerEventById (req, res, next) {
+  try {
+    let eventId = req.params.id
+    const event = await towerEventsService.getTowerEventById(eventId)
+    return res.send(event)
+  } catch (error) {
+    next(error)
+    }
+  }
+
 }
--- TowerReact/server/services/TowerEventsService.js
+++ TowerReact/server/services/TowerEventsService.js
@@ -1,6 +1,12 @@
 import { dbContext } from "../db/DbContext.js"
 
 class TowerEventsService{
+  async createTowerEvent(eventData) {
+    throw new Error("Method not implemented.")
+  }
+  async getTowerEventById(eventId) {
+    throw new Error("Method not implemented.")
+  }
   async getTowerEvents() {
     const events = await dbContext.TowerEvent.find().populate('creator', 'name picture')
     return events
